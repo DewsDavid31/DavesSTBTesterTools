572a34e5d2fe3382335d39eb32a939d9ea41610d
David Dews <davidjdews@gmail.com>
2022-08-08 08:30:11 -0600

    Now installs BVT files for DISH installer

--- Installer.bat
+++ Installer.bat
@@ -26,6 +26,10 @@ echo installing Demo config file...
 Start https://drive.google.com/u/0/uc?id=1XYm3a9kWeFrPdjTO8--gBt2LYG9tkW2F&export=download
 echo move the config file to the configs folder!!!
 pause
+echo installing BVT converter...
+start 'https://doc-00-2c-docs.googleusercontent.com/docs/securesc/vq0vpf5te81khc8omlhdqtvbu0i359nf/s28ajvup8nlmhdaqk9a20plbbema8a2t/1659968850000/07482902293360636346/07482902293360636346/16Sq7_nidZ1HpYTd7XIpIT67Tv3cyLNH8?e=download&ax=AI9vYm7b4UDP28P3bSzmipyVresHcDNmQYvbyzT-kUFW0dBBvFjyCZ-m4LLKIvMLMUmIrOPBFbbgHbAZ48SvZmT0AHBpsq_NTdTXdkqLF-ftKtxrw006_gE4N97Ui-MPFqpUEgDEgZ5J9AhLhyB9i39Tc2l4aJudLFgVyqeiOkGcmvuUOXnBevIiSkKKH82w9ozRNK2qOvxG0KSoBy_WPV70erAakPUAjZnyWrsNIC5Z1-yl7WZYErOC9-Ts77mmu7aIgwL4acINbfxg4gTJ4idRo86xPu7lF5iSBIge5p-RKob9WVCOVY85twJP8Fx31o4TbK4TykZYVxh1v_19xDtnqdU5dtMCSEhHiq07rS68HhIxgN8BQS8z57r3I1-3-cA4H-oHSkcTgLdQoXbm57Jh8BWzK74k67hPLwWXxhoGX1D8eCh4KUbjghL1maRxYDuhkcyKfReIcnHLcCkDGPGot6siGQsF_qs-Hpbfy5UWmSeUb0QKtWCuEhABsfPj0xEilba27eIGLkg7WCmIJ9g_6w5s7MpDj5Ayt3YS7H2FJVztaHzcGw4q56I58q4vPUBIgq9IjlRORjrRe1mWXhjYky4_5fk_nPFEyQWWl1wC5kj9gT-2s5ofuX0NjzdRqRQ4On_dHeNXjya40GGbPqOovgog-rAzqN45gbMRJCCs6j7MPhhBw3br05zZ0nJj5hRR9itNAErnQmNgTw&uuid=a22e2906-ca23-45e5-8725-cda6f2dedd10&authuser=0&nonce=pimgo67ul3kq4&user=07482902293360636346&hash=89risu16gsnoe1rtkasjvkpsekg4221u'
+echo move the downloaded ps1 file to the same folder DavesKeyBinder.bat is in...
+pause
 set index=0
 :AnotherBOX
 set /p serial=Enter the serial number of a box: 
