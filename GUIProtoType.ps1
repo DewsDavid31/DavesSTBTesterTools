9b4b89bdd59ddfb09cdfe4504be10817da325458
David Dews <davidjdews@gmail.com>
2022-09-30 09:52:54 -0600

    Progress, DO NOT USE THIS PS1

--- GUIProtoType.ps1
+++ GUIProtoType.ps1
@@ -44,6 +44,7 @@ $WTLabel = New-Object System.Windows.Forms.Label
 $WTLabel.Text = "Watches"
 $WTLabel.Location = New-Object System.Drawing.Point(0,150)
 $WTLabel.AutoSize = $true
+
 $WTButton = New-Object System.Windows.Forms.Button
 $WTButton.Location = New-Object System.Drawing.Size($AllHeight, 100)
 $WTButton.Size = New-Object System.Drawing.Size(120,23)
@@ -80,6 +81,10 @@ $WTProg5.Location = New-Object System.Drawing.point(400,150)
 $WTProg5.Minimum = 0
 $WTProg5.Maximum = 100
 
+$WTText = New-Object System.Windows.Forms.Label
+$WTText.Text = "Watch 0"
+$WTText.Location = New-Object System.Drawing.Point(200,180)
+$WTText.AutoSize = $true
 
 $BVLabel = New-Object System.Windows.Forms.Label
 $BVLabel.Text = "BVT"
@@ -89,12 +94,13 @@ $MGButton = New-Object System.Windows.Forms.Button
 $MGButton.Location = New-Object System.Drawing.Size($AllHeight, 200)
 $MGButton.Size = New-Object System.Drawing.Size(120,23)
 $MGButton.Text = "Merge CSVs"
-$BVCheck = New-Object System.Windows.Forms.CheckedListBox
+$BVCheck = New-Object System.Windows.Forms.ListBox
 $BVCheck.Width = 300
 $BVCheck.Location = New-Object System.Drawing.point(200,200)
 $BVCheck.Items.Add("BVT03.csv")
 $BVCheck.Items.Add("BVT02.csv")
 $BVCheck.Items.Add("BVT01.csv")
+
 $TRButton = New-Object System.Windows.Forms.Button
 $TRButton.Location = New-Object System.Drawing.Size($AllHeight, 230)
 $TRButton.Size = New-Object System.Drawing.Size(120,23)
@@ -111,14 +117,9 @@ $HKButton.Size = New-Object System.Drawing.Size(120,23)
 $HKButton.Text = "Bind Key"
 
 $UPButton = New-Object System.Windows.Forms.Button
-$UPButton.Text = "Unpair Key"
+$UPButton.Text = "Start Macro"
 $UPButton.Location = New-Object System.Drawing.Size($AllHeight,350)
 $UPButton.Size = New-Object System.Drawing.Size(120,23)
-
-$CHButton = New-Object System.Windows.Forms.Button
-$CHButton.Text = "Change Key"
-$CHButton.Location = New-Object System.Drawing.Size($AllHeight,380)
-$CHButton.Size = New-Object System.Drawing.Size(120,23)
 
 
 $HLLabel = New-Object System.Windows.Forms.Label
@@ -126,7 +127,7 @@ $HLLabel.Text = "Keys"
 $HLLabel.Location = New-Object System.Drawing.Point(200,310)
 $HLLabel.AutoSize = $true
 
-$HKList = New-Object System.Windows.Forms.CheckedListBox
+$HKList = New-Object System.Windows.Forms.ListBox
 $HKList.Width = 150
 $HKList.Location = New-Object System.Drawing.point(200,340)
 $HKList.Items.Add("+")
@@ -138,12 +139,17 @@ $SLLabel.Text = "Scripts"
 $SLLabel.Location = New-Object System.Drawing.Point(350,310)
 $SLLabel.AutoSize = $true
 
-$SCList = New-Object System.Windows.Forms.CheckedListBox
+$SCList = New-Object System.Windows.Forms.ListBox
 $SCList.Width = 150
 $SCList.Location = New-Object System.Drawing.point(350,340)
-$SCList.Items.Add("autocopier.ahk")
-$SCList.Items.Add("autopasser.ahk")
-$SCList.Items.Add("automux.ahk")
+$SCList.Items.Add("LockComputerWithPgUp.ahk")
+$SCList.Items.Add("AutoPasser.ahk")
+
+
+$Selected = New-Object System.Windows.Forms.Label
+$Selected.Text = "Nothing selected yet"
+$Selected.Location = New-Object System.Drawing.Point(200,500)
+$Selected.AutoSize = $true
 
 
 
@@ -163,10 +169,10 @@ $main_form.Controls.Add($DLButton)
 $main_form.Controls.Add($DLLabel)
 $main_form.Controls.Add($HKButton)
 $main_form.Controls.Add($UPButton)
-$main_form.Controls.Add($CHButton)
 $main_form.Controls.Add($HKLabel)
 $main_form.Controls.Add($WTButton)
 $main_form.Controls.Add($WTLabel)
+$main_form.Controls.Add($WTText)
 $main_form.Controls.Add($WTProg1)
 $main_form.Controls.Add($WTProg2)
 $main_form.Controls.Add($WTProg3)
@@ -182,13 +188,101 @@ $main_form.Controls.Add($HkList)
 $main_form.Controls.Add($BVCheck)
 $main_form.Controls.Add($BVLabel)
 $main_form.Controls.Add($TRButton)
+$main_form.Controls.Add($Selected)
+
+$CFButton.Add_Click(
+
+{
+}
+)
+$DLButton.Add_Click(
+
+{
+ for ($i = 0; $i - ($VRCheck.Items.Count); $i++)
+  {
+    if ($VRCheck.GetItemChecked($i))
+    { 
+    $a = $VRCheck.Items[$i].ToString();
+     for ($j = 0; $j - ($VRCheck.Items.Count); $j++)
+    {
+        if ($DLCheck.GetItemChecked($j))
+        { 
+            $s = $s + $DLCheck.Items[$j].ToString();
+            $s = $s + $a + ", "
+        }
+    }
+    }
+  }
+  $Selected.Text = "Downloading: " + $s
+}
+)
+$HKButton.Add_Click(
+
+{
+for ($i = 0; $i - ($HKList.Items.Count); $i++)
+  {
+    if ($HKList.GetItemChecked($i))
+    { 
+    $a = $HKList.Items[$i].ToString() + "::";
+     for ($j = 0; $j - ($SCList.Items.Count); $j++)
+    {
+        if ($SCList.GetItemChecked($j))
+        { 
+            $s = ".\MACRO\" + $SCList.Items[$j].ToString()
+            (Get-Content $s -Raw) -Replace [regex]::Escape("$a"),$CHText.Value | Set-Content $s
+            $HKList.Items[$i] = $a
+            start $s
+        }
+    }
+    }
+  }
+  $Selected.Text = $s
+}
+)
+$UPButton.Add_Click(
+
+{
+   
+}
+)
+
 $WTButton.Add_Click(
 
 {
-    $WTProg1.Value  = $WTProg1.Value + 10
+$WTText.Text = "AA1"
+$WTProg1.Value += 10
 }
-
 )
+$MGButton.Add_Click(
+
+{
+    $csvInmerged = ".\CSV\merged.csv"
+    $csvIn = ".\CSV\*.csv"
+    copy $csvIn  $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('sep=,'),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('sep=;'),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape(';'),[regex]::Escape(',') | Set-Content $csvInmerged
+    Import-Csv $csvInmerged -Delimiter ',' -Header "time","unused", "deviceName","unused2", "scenarioName", "unused3","status" | sort deviceName,scenarioName -Unique | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape(';'),[regex]::Escape(',') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('@{'),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('}'),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('time='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('unused='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('deviceName='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('unused2='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('scenarioName='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('unused3='),[regex]::Escape('') | Set-Content $csvInmerged
+    (Get-Content $csvInmerged -Raw) -Replace [regex]::Escape('status='),[regex]::Escape('') | Set-Content $csvInmerged
+}
+)
+$TRButton.Add_Click(
+
+{
+.\BVTConverter.ps1
+}
+)
+
+
 $main_form.ShowDialog()
 
 
