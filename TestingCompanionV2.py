45c5de9846799723917902407d8980ad9faa0dfb
ECHOSTAR\david.dews <david.dews@dish.com>
2022-12-16 13:55:02 -0700

    added tradefed support in multiple subprocesses

--- TestingCompanionV2.py
+++ TestingCompanionV2.py
@@ -227,7 +227,7 @@ class MacroHandler:
                 stdinalt.write(" ".join(args[5:]) + "\n")
                 for line in iter(stdoutalt.readline, ""):
                     print(host + ": "+ line)
-                    if "============================================" in line:
+                    if "============== End of Results ==============" in line:
                         break
                 client.close()
                 stdoutalt.close()
